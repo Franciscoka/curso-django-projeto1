5e2ff50c812b91a7b76f78cfd8d9983117977668
Franciscoka <franciscoka@hotmail.com>
2022-04-30 11:58:35 -0300

    identacao

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 # from recipes.views import home
 from . import views
 
+
 # recipes:recipe
 app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+
 ]
