0043ea3a47d9c81984891dc74b1da7464f49b67a
Franciscoka <franciscoka@hotmail.com>
2022-05-28 18:25:40 -0300

    Linux update

--- recipes/models.py
+++ recipes/models.py
@@ -17,17 +17,20 @@ class Recipe(models.Model):
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
-    preparation_step = models.TextField()
-    preparation_step_is_html = models.BooleanField(default=False)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_publish = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False)
     cover = models.ImageField(
-        upload_to='recipes/covers/%Y/%m/%d/')
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True)
+        User, on_delete=models.SET_NULL, null=True
+    )
 
     def __str__(self):
         return self.title
